79f3a3b610551a34964d109321e318134ccd717b
Ken <samoajoe9968@gmail.com>
2023-12-16 21:48:27 +0530

    Added advance CSS
    scroll bar added for text  length and added more functionality in the main program.

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,request,url_for
 import requests
-
-qury = 'hello'
 
 app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])  #app kaha bhejna hai
 def index():
-    API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-    api = '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'
-    headers = {"Authorization": f"Bearer {api}"}
+    return render_template('index.html')
+@app.route('/summarize',methods=["GET","POST"])
+def summarize():
+    if request.method =="POST":
 
-    def query(payload):
-        response = requests.post(API_URL, headers=headers, json=payload)
-        return response.json()
-    output = query({"inputs": f'{qury}'})
-    return render_template("index.html", result=output)  # redirect to homepage
+        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+        headers = {"Authorization": "Bearer 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"}
+
+        data = request.form['inputdata']
+        maxlen = int(request.form['maxl'])
+        minl = 100
+
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.json()
+        output = query({"inputs": f"{data}",
+                        "parameters":{"min_length":minl,"max_length":maxlen}})
+        return render_template("index.html",result=list(output)[0])
+    else:
+        return render_template('index.html')
 
 if __name__=='__main__':
     app.debug=True
